32b7404b972ffdbc94cbe1e5db66d2639004296f
Nikolai Emil Damm <nikolaiemildamm@icloud.com>
2023-01-05 20:21:37 +0100

    Updated dotnet commons to use my recommended test setup (#2)

---
+++ test/Devantler.Commons.StringHelpers.Test/StringExtensionsTests/ToCamelCaseTests.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Devantler.Commons.StringHelpers.Test.StringExtensionsTests;
+
+public class ToCamelCaseTests
+{
+    [Fact]
+    public void ToCamelCase_WithSingleWord_ReturnsCamelCase()
+    {
+        const string TEXT = "test";
+        const string EXPECTED = "test";
+        var actual = TEXT.ToCamelCase();
+        Assert.Equal(EXPECTED, actual);
+    }
+
+    [Fact]
+    public void ToCamelCase_WithMultipleWords_ReturnsCamelCase()
+    {
+        const string TEXT = "this is a test";
+        const string EXPECTED = "thisIsATest";
+        var actual = TEXT.ToCamelCase();
+        Assert.Equal(EXPECTED, actual);
+    }
+
+    [Fact]
+    public void ToCamelCase_WithPascalCase_ReturnsCamelCase()
+    {
+        const string TEXT = "ThisIsATest";
+        const string EXPECTED = "thisIsATest";
+        var actual = TEXT.ToCamelCase();
+        Assert.Equal(EXPECTED, actual);
+    }
+
+    [Fact]
+    public void ToCamelCase_WithSnakeCase_ReturnsCamelCase()
+    {
+        const string TEXT = "this_is_a_test";
+        const string EXPECTED = "thisIsATest";
+        var actual = TEXT.ToCamelCase();
+        Assert.Equal(EXPECTED, actual);
+    }
+
+    [Fact]
+    public void ToCamelCase_WithKebabCase_ReturnsCamelCase()
+    {
+        const string TEXT = "this-is-a-test";
+        const string EXPECTED = "thisIsATest";
+        var actual = TEXT.ToCamelCase();
+        Assert.Equal(EXPECTED, actual);
+    }
+}

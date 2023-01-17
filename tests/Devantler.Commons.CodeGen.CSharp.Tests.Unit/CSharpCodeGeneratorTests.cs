44688a027d003bbc3f266b8fe2561f2cac785d95
Nikolai Emil Damm <nikolaiemildamm@icloud.com>
2023-01-17 19:53:49 +0100

    Improved tests

--- tests/Devantler.Commons.CodeGen.CSharp.Tests.Unit/CSharpCodeGeneratorTests.cs
+++ tests/Devantler.Commons.CodeGen.CSharp.Tests.Unit/CSharpCodeGeneratorTests.cs
@@ -7,18 +7,15 @@ public class CSharpCodeGeneratorTests
 {
     [Theory]
     [MemberData(nameof(TestCases.ValidCases), MemberType = typeof(TestCases))]
-    public Task Generate_GivenValidCSharpCodeCollection_GenerateCodeCollection(string testName,
-        CSharpCompilation compilation)
+    public Task Generate_GivenValidCSharpCodeCollection_GenerateCodeCollection(string testName, CSharpCompilation compilation)
     {
         // Arrange
         var codeGenerator = new CSharpCodeGenerator();
 
         // Act
         var compiledCode = codeGenerator.Generate(compilation);
-
-        // Assert
         string result = string.Join(Environment.NewLine, compiledCode.Select(x => x.Value + Environment.NewLine));
-
+        // Assert
         return Verify(result).UseMethodName(testName);
     }
 

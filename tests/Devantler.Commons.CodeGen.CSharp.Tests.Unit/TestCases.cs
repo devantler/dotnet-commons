d301582d48aecd9fc84d2fb4267e5c2117c24868
Nikolai Emil Damm <nikolaiemildamm@icloud.com>
2023-01-17 01:10:37 +0100

    Added enum tests

--- tests/Devantler.Commons.CodeGen.CSharp.Tests.Unit/TestCases.cs
+++ tests/Devantler.Commons.CodeGen.CSharp.Tests.Unit/TestCases.cs
@@ -13,7 +13,10 @@ public static class TestCases
             new object[] { nameof(CSharpCodeCollectionFactory.CreateCSharpCodeCollectionStubWithOnePopulatedClassWithMultipleMembers), CSharpCodeCollectionFactory.CreateCSharpCodeCollectionStubWithOnePopulatedClassWithMultipleMembers() },
             new object[] { nameof(CSharpCodeCollectionFactory.CreateCSharpCodeCollectionStubWithOneEmptyInterface), CSharpCodeCollectionFactory.CreateCSharpCodeCollectionStubWithOneEmptyInterface() },
             new object[] { nameof(CSharpCodeCollectionFactory.CreateCSharpCodeCollectionStubWithOnePopulatedInterfaceWithSingleMembers), CSharpCodeCollectionFactory.CreateCSharpCodeCollectionStubWithOnePopulatedInterfaceWithSingleMembers() },
-            new object[] { nameof(CSharpCodeCollectionFactory.CreateCSharpCodeCollectionStubWithOnePopulatedInterfaceWithMultipleMembers), CSharpCodeCollectionFactory.CreateCSharpCodeCollectionStubWithOnePopulatedInterfaceWithMultipleMembers() }
+            new object[] { nameof(CSharpCodeCollectionFactory.CreateCSharpCodeCollectionStubWithOnePopulatedInterfaceWithMultipleMembers), CSharpCodeCollectionFactory.CreateCSharpCodeCollectionStubWithOnePopulatedInterfaceWithMultipleMembers() },
+            new object[] { nameof(CSharpCodeCollectionFactory.CreateCSharpCodeCollectionStubWithOneEmptyEnum), CSharpCodeCollectionFactory.CreateCSharpCodeCollectionStubWithOneEmptyEnum() },
+            new object[] { nameof(CSharpCodeCollectionFactory.CreateCSharpCodeCollectionStubWithOnePopulatedEnumWithSingleValue), CSharpCodeCollectionFactory.CreateCSharpCodeCollectionStubWithOnePopulatedEnumWithSingleValue() },
+            new object[] { nameof(CSharpCodeCollectionFactory.CreateCSharpCodeCollectionStubWithOnePopulatedEnumWithMultipleValues), CSharpCodeCollectionFactory.CreateCSharpCodeCollectionStubWithOnePopulatedEnumWithMultipleValues() },
         };
 
     public static IEnumerable<object[]> InvalidCases =>

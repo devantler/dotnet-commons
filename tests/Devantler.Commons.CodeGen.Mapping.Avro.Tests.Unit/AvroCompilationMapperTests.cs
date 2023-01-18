7479aafe02d8985ca74db30ab2b0781efd1194ef
Nikolai Emil Damm <nikolaiemildamm@icloud.com>
2023-01-18 23:01:14 +0100

    Added tests for Avro.Mapping (#15)
    * Added tests for Avro.Mapping
    
    * fixed test
    
    * Added missing tests
    
    * Removed unneeded serialization
    
    * Updated verifier results

---
+++ tests/Devantler.Commons.CodeGen.Mapping.Avro.Tests.Unit/AvroCompilationMapperTests.cs
@@ -0,0 +1,37 @@
+using System.Text.Json;
+using Avro;
+using Devantler.Commons.CodeGen.Core;
+
+namespace Devantler.Commons.CodeGen.Mapping.Avro.Tests.Unit;
+
+[UsesVerify]
+public class AvroCompilationMapperTests
+{
+    [Theory]
+    [MemberData(nameof(TestCases.ValidCases), MemberType = typeof(TestCases))]
+    public Task Map_GivenValidSchema_ReturnsValidCompilation(Schema schema, Language language)
+    {
+        // Arrange
+        var mapper = new AvroCompilationMapper();
+
+        // Act
+        var compilation = mapper.Map(schema, language);
+
+        // Assert
+        return Verify(compilation).UseMethodName(schema.Tag.ToString());
+    }
+
+    [Fact]
+    public void Map_GivenUnsupportedLanguage_ThrowsNotSupportedException()
+    {
+        // Arrange
+        const Language language = (Language)999;
+        var mapper = new AvroCompilationMapper();
+
+        // Act
+        var action = () => mapper.Map(RecordSchema.Create("EmptyShema", new List<Field>()), language);
+
+        // Assert
+        _ = action.Should().Throw<NotSupportedException>();
+    }
+}

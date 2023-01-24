2d3e57d61bf4961250149cd479eec59f882af6ca
Nikolai Emil Damm <nikolaiemildamm@icloud.com>
2023-01-24 17:25:55 +0100

    v0.1.1

--- src/Devantler.Commons.CodeGen.Core/Interfaces/ICodeGenerator.cs
+++ src/Devantler.Commons.CodeGen.Core/Interfaces/ICodeGenerator.cs
@@ -3,13 +3,12 @@ namespace Devantler.Commons.CodeGen.Core.Interfaces;
 /// <summary>
 ///     An interface representing a code generator.
 /// </summary>
-/// <typeparam name="T"></typeparam>
-public interface ICodeGenerator<T> where T : ICompilation
+public interface ICodeGenerator
 {
     /// <summary>
     ///     Generates code from the given code base.
     /// </summary>
     /// <param name="compilation"></param>
     /// <returns>Dictionary&lt;fileName, code&gt;</returns>
-    public Dictionary<string, string> Generate(T compilation);
+    public Dictionary<string, string> Generate(ICompilation compilation);
 }

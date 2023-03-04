7ab5633b9ad48fd6890e81600796132851c656b8
Nikolai Emil Damm <nikolaiemildamm@icloud.com>
2023-03-04 16:02:06 +0100

    Extend the Flatten method to recursively flatten schemas

--- src/Devantler.Commons.CodeGen.Mapping.Avro/Extensions/SchemaExtensions.cs
+++ src/Devantler.Commons.CodeGen.Mapping.Avro/Extensions/SchemaExtensions.cs
@@ -18,11 +18,30 @@ public static class SchemaExtensions
             List<Schema> schemas = new();
 
             if (schema is RecordSchema recordSchema)
+            {
                 schemas.Add(recordSchema);
+                foreach (var field in recordSchema.Fields)
+                {
+                    schemas.AddRange(Flatten(field.Type));
+                }
+            }
             else if (schema is EnumSchema enumSchema)
+            {
                 schemas.Add(enumSchema);
+            }
+            else if (schema is ArraySchema arraySchema)
+            {
+                schemas.AddRange(Flatten(arraySchema.Item));
+            }
+            else if (schema is MapSchema mapSchema)
+            {
+                schemas.AddRange(Flatten(mapSchema.Value));
+            }
             else if (schema is UnionSchema unionSchema)
+            {
                 schemas.AddRange(unionSchema.Schemas.ToList().SelectMany(Flatten));
+            }
+
             return schemas;
         }
 

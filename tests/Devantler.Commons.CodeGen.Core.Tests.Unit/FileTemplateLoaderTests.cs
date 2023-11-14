5d0515ce05a26f7f25807f18d7d78f6787eafb09
Nikolai Emil Damm <nikolaiemildamm@icloud.com>
2023-11-14 20:51:18 +0100

    Add GenerateDocumentationFile property and unit tests for FileTemplateLoader class (#228)
    * Add GenerateDocumentationFile property and update
    package references in
    Devantler.Commons.CodeGen.Core.Tests.Unit.csproj.
    Add unit tests for FileTemplateLoader class.
    
    * Update CodeCov Action to Upload Coverage Report

--- tests/Devantler.Commons.CodeGen.Core.Tests.Unit/FileTemplateLoaderTests.cs
+++ tests/Devantler.Commons.CodeGen.Core.Tests.Unit/FileTemplateLoaderTests.cs
@@ -5,8 +5,16 @@ using Scriban.Parsing;
 
 namespace Devantler.Commons.CodeGen.Core.Tests.Unit;
 
+/// <summary>
+/// Unit tests for the <see cref="FileTemplateLoader"/> class.
+/// </summary>
 public class FileTemplateLoaderTests
 {
+    /// <summary>
+    /// Tests if the GetPath method returns a valid path when given a template name.
+    /// </summary>
+    /// <param name="context">The template context.</param>
+    /// <param name="sourceSpan">The source span.</param>
     [Theory, AutoNSubstituteData(typeof(SupportMutableValueTypesCustomization))]
     public void GetPath_GivenTemplateName_ReturnsValidPath(TemplateContext context, SourceSpan sourceSpan)
     {
@@ -20,6 +28,11 @@ public class FileTemplateLoaderTests
         Assert.Equal($"{Directory.GetCurrentDirectory()}/templates/test-template.sbn", result);
     }
 
+    /// <summary>
+    /// Tests the Load method of the FileTemplateLoader class when given a valid path and ensures that it returns the contents of the file.
+    /// </summary>
+    /// <param name="context">The template context to use for loading the file.</param>
+    /// <param name="sourceSpan">The source span to use for loading the file.</param>
     [Theory, AutoNSubstituteData(typeof(SupportMutableValueTypesCustomization))]
     public void Load_GivenValidPath_ReturnsFileContents(TemplateContext context, SourceSpan sourceSpan)
     {
@@ -34,6 +47,11 @@ public class FileTemplateLoaderTests
         Assert.Equal("This is a test template.", result);
     }
 
+    /// <summary>
+    /// Tests that an exception is thrown when attempting to load a template from an invalid file path.
+    /// </summary>
+    /// <param name="context">The template context.</param>
+    /// <param name="sourceSpan">The source span.</param>
     [Theory, AutoNSubstituteData(typeof(SupportMutableValueTypesCustomization))]
     public void Load_GivenInvalidPath_ThrowsFileNotFoundException(TemplateContext context, SourceSpan sourceSpan)
     {
@@ -48,6 +66,12 @@ public class FileTemplateLoaderTests
         _ = Assert.Throws<FileNotFoundException>(action);
     }
 
+    /// <summary>
+    /// Tests that LoadAsync returns the contents of the file when given a valid path.
+    /// </summary>
+    /// <param name="context"></param>
+    /// <param name="sourceSpan"></param>
+    /// <returns></returns>
     [Theory, AutoNSubstituteData(typeof(SupportMutableValueTypesCustomization))]
     public async Task LoadAsync_GivenValidPath_ReturnsFileContents(TemplateContext context, SourceSpan sourceSpan)
     {
@@ -62,8 +86,14 @@ public class FileTemplateLoaderTests
         Assert.Equal("This is a test template.", result);
     }
 
+    /// <summary>
+    /// Tests that LoadAsync throws a FileNotFoundException when given an invalid path.
+    /// </summary>
+    /// <param name="context"></param>
+    /// <param name="sourceSpan"></param>
+    /// <returns></returns>
     [Theory, AutoNSubstituteData(typeof(SupportMutableValueTypesCustomization))]
-    public void LoadAsync_GivenInvalidPath_ThrowsFileNotFoundException(TemplateContext context, SourceSpan sourceSpan)
+    public async Task LoadAsync_GivenInvalidPath_ThrowsFileNotFoundException(TemplateContext context, SourceSpan sourceSpan)
     {
         // Arrange
         var sut = new FileTemplateLoader();
@@ -73,6 +103,6 @@ public class FileTemplateLoaderTests
         async Task action() => await sut.LoadAsync(context, sourceSpan, path);
 
         // Assert
-        _ = Assert.ThrowsAsync<FileNotFoundException>(action);
+        _ = await Assert.ThrowsAsync<FileNotFoundException>(action);
     }
 }

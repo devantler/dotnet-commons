c2b41f6a18b1529857a0814ce07d650ddedcb54e
Nikolai Emil Damm <nikolaiemildamm@icloud.com>
2023-11-30 17:01:01 +0100

    Update project configurations (#242)

--- tests/Devantler.Commons.CodeGen.CSharp.Tests.Unit/CSharpCodeGeneratorTests.cs
+++ tests/Devantler.Commons.CodeGen.CSharp.Tests.Unit/CSharpCodeGeneratorTests.cs
@@ -3,9 +3,17 @@ using Devantler.Commons.CodeGen.CSharp.Tests.Unit.TestCases;
 
 namespace Devantler.Commons.CodeGen.CSharp.Tests.Unit;
 
+/// <summary>
+/// Tests for <see cref="CSharpCodeGenerator"/>.
+/// </summary>
 [UsesVerify]
 public class CSharpCodeGeneratorTests
 {
+    /// <summary>
+    /// Tests the <see cref="CSharpCodeGenerator.Generate"/> method.
+    /// </summary>
+    /// <param name="testName"></param>
+    /// <param name="compilation"></param>
     [Theory]
     [MemberData(nameof(ClassTestCases.ValidCases), MemberType = typeof(ClassTestCases))]
     [MemberData(nameof(InterfaceTestCases.ValidCases), MemberType = typeof(InterfaceTestCases))]
--- tests/Devantler.Commons.CodeGen.Mapping.Avro.Tests.Unit/Mappers/AvroModelsCompilationMapperTests/TestCases.cs
+++ tests/Devantler.Commons.CodeGen.Mapping.Avro.Tests.Unit/Mappers/AvroModelsCompilationMapperTests/TestCases.cs
@@ -23,7 +23,7 @@ public static class TestCases
                             GetRecordSchema("UnionRecordSchema1"),
                             new RecordSchema("UnionRecordSchema2", new List<RecordField>
                             {
-                                new RecordField("EnumField", enumSchema) { Documentation = "Test documentation" },
+                                new("EnumField", enumSchema) { Documentation = "Test documentation" },
                             }) { Documentation = "Test documentation" },
                             enumSchema
                     }), Language.CSharp
@@ -36,16 +36,16 @@ public static class TestCases
     {
         return new RecordSchema(name, new List<RecordField>
             {
-                new RecordField("IntField", new IntSchema()) { Documentation = "Test documentation", Default = new ObjectDefaultValue<int>(0, new IntSchema()) },
-                new RecordField("StringField", new StringSchema()) { Documentation = "Test documentation", Default = new ObjectDefaultValue<string>("string", new StringSchema()) },
-                new RecordField("BooleanField", new BooleanSchema()) { Documentation = "Test documentation", Default = new ObjectDefaultValue<bool>(false, new BooleanSchema()) },
-                new RecordField("DoubleField", new DoubleSchema()) { Documentation = "Test documentation", Default = new ObjectDefaultValue<double>(1.5, new DoubleSchema()) },
-                new RecordField("FloatField", new FloatSchema()) { Documentation = "Test documentation", Default = new ObjectDefaultValue<float>(1.5f, new FloatSchema()) },
-                new RecordField("LongField", new LongSchema()) { Documentation = "Test documentation", Default = new ObjectDefaultValue<long>(10L, new LongSchema()) },
-                new RecordField("NullField", new UnionSchema(new List<Schema>(){new StringSchema(), new NullSchema()})) { Documentation = "Test documentation", Default = new ObjectDefaultValue<object>(null!, new NullSchema()) },
-                new RecordField("BytesField", new BytesSchema()) { Documentation = "Test documentation", Default = new ObjectDefaultValue<byte[]>(new byte[1], new BytesSchema()) },
-                new RecordField("StringArrayField", new ArraySchema(new StringSchema())) { Documentation = "Test documentation" },
-                new RecordField("StringMapField", new MapSchema(new StringSchema())) { Documentation = "Test documentation" }
+                new("IntField", new IntSchema()) { Documentation = "Test documentation", Default = new ObjectDefaultValue<int>(0, new IntSchema()) },
+                new("StringField", new StringSchema()) { Documentation = "Test documentation", Default = new ObjectDefaultValue<string>("string", new StringSchema()) },
+                new("BooleanField", new BooleanSchema()) { Documentation = "Test documentation", Default = new ObjectDefaultValue<bool>(false, new BooleanSchema()) },
+                new("DoubleField", new DoubleSchema()) { Documentation = "Test documentation", Default = new ObjectDefaultValue<double>(1.5, new DoubleSchema()) },
+                new("FloatField", new FloatSchema()) { Documentation = "Test documentation", Default = new ObjectDefaultValue<float>(1.5f, new FloatSchema()) },
+                new("LongField", new LongSchema()) { Documentation = "Test documentation", Default = new ObjectDefaultValue<long>(10L, new LongSchema()) },
+                new("NullField", new UnionSchema(new List<Schema>(){new StringSchema(), new NullSchema()})) { Documentation = "Test documentation", Default = new ObjectDefaultValue<object>(null!, new NullSchema()) },
+                new("BytesField", new BytesSchema()) { Documentation = "Test documentation", Default = new ObjectDefaultValue<byte[]>(new byte[1], new BytesSchema()) },
+                new("StringArrayField", new ArraySchema(new StringSchema())) { Documentation = "Test documentation" },
+                new("StringMapField", new MapSchema(new StringSchema())) { Documentation = "Test documentation" }
             })
         { Documentation = "Test documentation" };
     }

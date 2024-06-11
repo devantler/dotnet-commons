6634b84df6bd2ce81808bfcaec8ba420d184bed5
devantler <nikolaiemildamm@icloud.com>
2024-06-11 23:58:20 +0200

    chore: Update Casing enum and RegexLibrary in StringHelpers

--- src/Devantler.Commons.StringHelpers/Casing.cs
+++ src/Devantler.Commons.StringHelpers/Casing.cs
@@ -5,28 +5,28 @@ namespace Devantler.Commons.StringHelpers;
 /// </summary>
 public enum Casing
 {
-    /// <summary>
-    /// PascalCase
-    /// </summary>
-    Pascal,
-    /// <summary>
-    /// camelCase
-    /// </summary>
-    Camel,
-    /// <summary>
-    /// kebab-case
-    /// </summary>
-    Kebab,
-    /// <summary>
-    /// Title Case
-    /// </summary>
-    Title,
-    /// <summary>
-    /// UPPER CASE
-    /// </summary>
-    Upper,
-    /// <summary>
-    /// lower case
-    /// </summary>
-    Lower
+  /// <summary>
+  /// PascalCase
+  /// </summary>
+  Pascal,
+  /// <summary>
+  /// camelCase
+  /// </summary>
+  Camel,
+  /// <summary>
+  /// kebab-case
+  /// </summary>
+  Kebab,
+  /// <summary>
+  /// Title Case
+  /// </summary>
+  Title,
+  /// <summary>
+  /// UPPER CASE
+  /// </summary>
+  Upper,
+  /// <summary>
+  /// lower case
+  /// </summary>
+  Lower
 }
